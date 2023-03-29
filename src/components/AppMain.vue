25abcb97bc0500627130035791ea411a3796c7a7
christianarca27 <christian.arca.mail@gmail.com>
2023-03-29 18:57:58 +0200

    feat: inserita barra di ricerca

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,6 +16,11 @@ export default {
 <template>
     <main>
         <div class="container">
+            <div id="form">
+                <input type="text" placeholder="Inserisci parole chiave...">
+                <button>Cerca</button>
+            </div>
+
             <MainBriefcase>
             </MainBriefcase>
         </div>
@@ -37,6 +42,34 @@ main {
         max-height: 100%;
         aspect-ratio: 1/1.4;
         padding: 50px;
+
+        #form {
+            width: 100vw;
+            height: 5vh;
+
+            display: flex;
+
+            position: fixed;
+            top: 0;
+            left: 0;
+
+            input {
+                display: block;
+                width: 80%;
+                height: 100%;
+                padding: 0 1rem;
+
+                font-size: 1.2rem;
+            }
+
+            button {
+                display: block;
+                width: 20%;
+                height: 100%;
+
+                font-size: 1.2rem;
+            }
+        }
     }
 }
 </style>
